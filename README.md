2a2f15923beafc1dedb8261446728391ceb3f754
Ali Ok <aliok@redhat.com>
2021-03-29 01:26:41 +0300

    CRC

--- README.md
+++ README.md
@@ -11,7 +11,22 @@ To be placed at `~/host`.
 Start in guest:
 
 ```
-~/start-kind.sh
+start-kind.sh
 ```
 
 Dashboard doesn't work. See https://stackoverflow.com/questions/53957413/how-to-access-kubernetes-dashboard-from-outside-network
+
+
+## crc
+
+The official guide [here](https://code-ready.github.io/crc/#setting-up-remote-server_gsg) shows how to setup crc on the host and how to connect 
+to it from a Fedora guest. However, in my case, my guest is a Mac. There is [another](https://www.opensourcerers.org/2021/03/22/accessing-a-remote-codeready-containers-installation-with-macos/) guide for that.
+
+What I did:
+- Do the things in the official guide(skipped the firewall shit, only did the HA proxy part) (I actaually disabled firewall entirely)
+- Do the things in the 2nd article - optimized setup: install dnsmasq on host, configure /etc/resolver on Mac to point to host. UPDATE: this didn't work. There is another dnsmasq instance on Linux host which binds to port 53 and I can't change its config. I tried starting a 2nd instance of dnsmasq at a custom port, but Mac can't connect to dns server with custom port. In the end, I just did't do the optimized setup. I installed dnsmasq on my Mac and pointed at that in Mac's dns settings. It resolves to this machine.
+
+Start in guest:
+```
+start-crc.sh
+```
---
+++ start-crc.sh
@@ -0,0 +1,77 @@
+#/bin/bash
+
+# Exit on error
+set -e
+
+# This needs increasing the timeout in /etc/sudoers
+echo "Need sudo"
+sudo echo "Ok"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+source ${DIR}/common.sh
+
+header_text "Deleting current cluster"
+crc delete || true
+
+header_text "Creating a new cluster"
+crc start --pull-secret-file=${DIR}/crc-pull-secret.txt
+
+header_text "Printing cluster information"
+kubectl cluster-info --context crc-admin
+
+header_text "Extracting KUBECONFIG into ${DIR}/crc.kubeconfig"
+kubectl config view --minify --flatten --context=crc-admin > ${DIR}/crc.kubeconfig
+
+header_text "Setting up HAProxy"
+export CRC_IP=$(crc ip)
+sudo tee /etc/haproxy/haproxy.cfg &>/dev/null <<EOF
+global
+    debug
+
+defaults
+    log global
+    mode http
+    timeout connect 5000
+    timeout client 500000
+    timeout server 500000
+
+frontend apps
+    bind 0.0.0.0:80
+    option tcplog
+    mode tcp
+    default_backend apps
+
+frontend apps_ssl
+    bind 0.0.0.0:443
+    option tcplog
+    mode tcp
+    default_backend apps_ssl
+
+backend apps
+    mode tcp
+    balance roundrobin
+    server webserver1 $CRC_IP:80 check
+
+backend apps_ssl
+    mode tcp
+    balance roundrobin
+    option ssl-hello-chk
+    server webserver1 $CRC_IP:443 check
+
+frontend api
+    bind 0.0.0.0:6443
+    option tcplog
+    mode tcp
+    default_backend api
+
+backend api
+    mode tcp
+    balance roundrobin
+    option ssl-hello-chk
+    server webserver1 $CRC_IP:6443 check
+EOF
+
+header_text "Starting haproxy"
+sudo systemctl start haproxy
+
